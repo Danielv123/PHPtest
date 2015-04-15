2af9619b864add5ca59b68278c59e1cd4c2a44fb
Danielv123 <tranen.bananen@gmail.com>
2015-04-15 08:07:58 +0200

    Uploads .php updated, files tested

--- upload.php
+++ upload.php
@@ -30,7 +30,8 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.<br>";
+		echo ($target_file);
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
